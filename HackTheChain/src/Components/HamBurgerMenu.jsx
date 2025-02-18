43ded541710564853997e040633f6c460e03abf3
yashag710 <yashag1810@gmail.com>
2025-02-18 22:02:08 +0530

    logo changed

--- HackTheChain/src/Components/HamBurgerMenu.jsx
+++ HackTheChain/src/Components/HamBurgerMenu.jsx
@@ -20,10 +20,10 @@ const HamburgerMenu = () => {
           <img
             src="/pictures/HTC_logo.png"
             alt="Logo"
-            className="h-10 w-10 md:h-12 md:w-12 lg:h-14 lg:w-14"
+            className="h-14 w-25 md:h-12 md:w-12 lg:h-14 lg:w-14"
           />
 
-          <div className="text-2xl font-bold tracking-tighter">HackTheChain3.0</div>
+          <div className="text-2xl font-bold tracking-tighter header">HackTheChain3.0</div>
         </div>
 
         <button
